f7f2c72126a52f4661c4bc51a7ff36a79a0a31c9
zJokyy <93003919+zJokyy@users.noreply.github.com>
2021-11-19 09:22:15 +0200

    Create 2D array.cs

---
+++ 2D array.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Diagnostics;
+
+namespace Struuct
+{
+        class Program
+     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter  the number of rows ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter  the number of columns ");
+            int columns = int.Parse(Console.ReadLine());
+            int[,] Arr = new int[rows, columns];
+            for(int i = 0; i < Arr.GetLength(0); i++)//Read input
+            {
+                for(int j = 0; j < Arr.GetLength(1);j++)
+                {
+                    Console.WriteLine("Enter {0},{1} Element " , i , j);
+                    Arr[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for(int i = 0; i < Arr.GetLength(0); i++)//Print output
+            {
+                for(int j = 0; j < Arr.GetLength(1);j++)
+                {
+                    Console.Write(Arr[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
