5ae7f9174e8c75524c0f4ea4c5debebbda5d2c72
zJokyy <93003919+zJokyy@users.noreply.github.com>
2021-11-19 09:52:20 +0200

    Create Symmetric Array.cs

---
+++ Symmetric Array.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Symmetric_Array
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of array : ");
+            int x = int.Parse(Console.ReadLine());
+            int[] arr = new int[x];
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Enter the {0} element : ", i);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            bool symmetric = true;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != arr[arr.Length - i - 1])
+                {
+                    symmetric= false;
+                    break;
+                }
+            }
+            if(symmetric)
+                 Console.WriteLine("Symmetric"); 
+            else
+                Console.WriteLine("Not Symmetric");
+        }
+    }
+    }
