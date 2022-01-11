f81c1703e94eb71f6a8b6ce9b41f868571180ac0
Mohammed Abdulsayed <93003919+Abdulsayedd@users.noreply.github.com>
2022-01-11 09:24:21 +0200

    Symmetric Array

--- Symmetric Array.cs
+++ Symmetric Array.cs
@@ -24,7 +24,7 @@ namespace Symmetric_Array
                 }
             }
             if(symmetric)
-                 Console.WriteLine("Symmetric"); 
+                Console.WriteLine("Symmetric"); 
             else
                 Console.WriteLine("Not Symmetric");
         }
